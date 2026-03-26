1ce59d8d4d4990dce3dff8e25befaa04e45cc1d2
bobhatevishal <bobhatev13@gmail.com.com>
2026-03-26 06:21:01 +0000

    update modules

--- infrastructure/modules/aca_app/outputs.tf
+++ infrastructure/modules/aca_app/outputs.tf
@@ -1,9 +1,7 @@
 output "id" {
-  description = "The ID of the Container App"
   value       = azurerm_container_app.app.id
 }
 
 output "fqdn" {
-  description = "The FQDN of the Container App Ingress"
   value       = azurerm_container_app.app.ingress[0].fqdn
 }
